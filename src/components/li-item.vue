81768c0ed41ca2aa4a0a1156381d346af3129e3d
Juan Santos <al16760563@gmail.com>
2021-01-01 03:03:06 -0800

    Update settign basic

---
+++ src/components/li-item.vue
@@ -0,0 +1,10 @@
+<template>
+    <li>{{ data.text }}</li>
+</template>
+
+<script>
+export default {
+    name: "todo-item",
+    props: ["data"]
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!--
+      <img alt="Vue logo" src="../assets/logo.png" />
+      <HelloWorld msg="Welcome to Your Vue.js App" />
+    -->
+    <todo-item v-for="cosa in cosas"
+      v-bind:data="cosa"
+      v-bind:key="cosa.id"
+    >
+    </todo-item>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+//import HelloWorld from "@/components/HelloWorld.vue";
+
+import todoItem from "@/components/li-item.vue";
 
 export default {
   name: "Home",
+  data() {
+    return {
+      cosas: [
+      { id: 0, text: "Vegetales" },
+      { id: 1, text: "Frutas" },
+      { id: 2, text: "Carne" }
+    ]
+    }
+  },
   components: {
-    HelloWorld
+    todoItem
   }
 };
 </script>
